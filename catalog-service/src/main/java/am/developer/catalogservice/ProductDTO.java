6c6a3cfbf0aff56b3c6d4eca00e67da643ea9bbd
haykh <Hayastan21>
2018-11-13 11:05:28 +0400

    init

---
+++ catalog-service/src/main/java/am/developer/catalogservice/ProductDTO.java
@@ -0,0 +1,15 @@
+package am.developer.catalogservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private double price;
+    private String promotion;
+}
